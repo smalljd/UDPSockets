0934a50462e3745b76eaa9e9770e976db1f3bada
Jeff Small <Jeff@JeffsMacBook.local>
2018-01-21 17:35:14 -0800

    Start up a server on app launch, and close the socket on termination

--- UDPSockets/AppDelegate.swift
+++ UDPSockets/AppDelegate.swift
@@ -10,15 +10,13 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    var server: UDPServer?
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        server = UDPServer(address: "127.0.0.1", port: 6600)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        server?.closeSocket()
     }
 
 
